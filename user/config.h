3d7c427a501413efe6091e03db904977e9a55093
Noah Frederick <code@noahfrederick.com>
2018-11-13 08:16:04 -0500

    Play sound when switching default layer
    This used to work, but when it was removed from the default keymap I
    didn't notice I had to make this addition to the config.h file.

--- user/config.h
+++ user/config.h
@@ -4,3 +4,9 @@
 // and it saves on space in the firmware.
 #define NO_ACTION_MACRO
 #define NO_ACTION_FUNCTION
+
+#ifdef AUDIO_ENABLE
+// Play sounds when the default layer is set.
+#define DEFAULT_LAYER_SONGS { SONG(QWERTY_SOUND), \
+                              SONG(COLEMAK_SOUND) }
+#endif
