e8f1f717afc2cd55f00482996926598f17293c72
Noah Frederick <code@noahfrederick.com>
2019-04-12 15:41:36 -0400

    Fix "make" macro for Preonic

--- user/my.c
+++ user/my.c
@@ -101,7 +101,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case SEND_MAKE:
       if (record->event.pressed) {
         SEND_STRING("make " QMK_KEYBOARD ":" QMK_KEYMAP
-#if (defined(BOOTLOADER_DFU) || defined(BOOTLOADER_LUFA_DFU) || defined(BOOTLOADER_QMK_DFU))
+#if defined(__arm__)
+            ":dfu-util"
+#elif (defined(BOOTLOADER_DFU) || defined(BOOTLOADER_LUFA_DFU) || defined(BOOTLOADER_QMK_DFU))
             ":dfu"
 #elif defined(BOOTLOADER_HALFKAY)
             ":teensy"
