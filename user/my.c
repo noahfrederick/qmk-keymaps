e148a852079968a5054a4bf750090422678e5960
Noah Frederick <code@noahfrederick.com>
2019-09-29 08:42:57 -0400

    Simplify "make" macro
    The `:flash` make target is undocumented but seems to work.

--- user/my.c
+++ user/my.c
@@ -105,17 +105,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       return false;
     case SEND_MAKE:
       if (record->event.pressed) {
-        SEND_STRING("make " QMK_KEYBOARD ":" QMK_KEYMAP
-#if defined(__arm__)
-            ":dfu-util"
-#elif (defined(BOOTLOADER_DFU) || defined(BOOTLOADER_LUFA_DFU) || defined(BOOTLOADER_QMK_DFU))
-            ":dfu"
-#elif defined(BOOTLOADER_HALFKAY)
-            ":teensy"
-#elif defined(BOOTLOADER_CATERINA)
-            ":avrdude"
-#endif
-            SS_TAP(X_ENTER));
+        SEND_STRING("make " QMK_KEYBOARD ":" QMK_KEYMAP ":flash" SS_TAP(X_ENTER));
       }
       return false;
   }
